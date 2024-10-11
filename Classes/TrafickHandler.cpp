f1581c41fe2eaae43bd4cda08f64ecd24c37f147
xpetra31 <xpetra31@Laptop>
2024-10-11 17:40:46 +0200

    FIX - Updated err messages

--- Classes/TrafickHandler.cpp
+++ Classes/TrafickHandler.cpp
@@ -18,7 +18,7 @@ private:
     };
 
     static const int MAX_STATS = 100;
-    RuntimeConfiguration* configuration;  // Uchováváme název rozhraní
+    RuntimeConfiguration* configuration; 
     traffic_stat stats[MAX_STATS];
     int stats_count = 0;
     int keep_running = 1;
@@ -89,54 +89,43 @@ public:
         }
     }
 
-    // Tato metoda byla přejmenována z 'main' na 'run'
     int run() 
     {
         char errbuf[PCAP_ERRBUF_SIZE];
         struct bpf_program fp;
         char filter_exp[] = "ip";
-        bpf_u_int32 net = 0;  // Declare as regular variable, not a pointer
-        bpf_u_int32 mask = 0; // Variable to store the network mask
+        bpf_u_int32 net = 0;  
+        bpf_u_int32 mask = 0; 
 
         instance = this;
 
-        signal(SIGINT, handle_sigint_static);  // Set up signal handler for Ctrl+C
+        signal(SIGINT, handle_sigint_static);  
 
-        // Get the network and mask using pcap_lookupnet
         if (pcap_lookupnet(configuration->Interface, &net, &mask, errbuf) == -1) {
             fprintf(stderr, "Nemohu získat netmask pro zařízení: %s\n", errbuf);
-            net = 0;  // Default to zero if lookup fails
+            net = 0;  
         }
 
-        // Open the device for capturing packets using the interface from configuration
         handle = pcap_open_live(configuration->Interface, BUFSIZ, 1, 1000, errbuf);
         if (handle == nullptr) {
-            fprintf(stderr, "Nemohu otevřít zařízení: %s\n", errbuf);
+            fprintf(stderr, "ERR: %s\n", errbuf);
             return 2;
         }
 
-        // Compile the filter expression into a BPF program
         if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-            fprintf(stderr, "Nemohu kompilovat filtr: %s\n", pcap_geterr(handle));
+            fprintf(stderr, "ERR: %s\n", pcap_geterr(handle));
             return 2;
         }
-
-        // Set the compiled filter to the pcap handle
         if (pcap_setfilter(handle, &fp) == -1) {
-            fprintf(stderr, "Nemohu nastavit filtr: %s\n", pcap_geterr(handle));
+            fprintf(stderr, "ERR: %s\n", pcap_geterr(handle));
             return 2;
         }
-
-        // Capture packets in a loop until interrupted
         while (keep_running) {
             pcap_dispatch(handle, 10, packet_handler_static, NULL);
             display_stats();
             sleep(1);
         }
-
-        // Close the pcap handle and clean up
         pcap_close(handle);
-        printf("\nProgram byl ukončen.\n");
         return 0;
     }
 
